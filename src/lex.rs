3d5c49b0c9d5f9ddf375b2d322215bb318423ed2
Reines <reines@Reiness-MacBook-Pro.local>
2025-06-02 19:23:28 +0100

    Initial commit
    Initial commit

---
+++ src/lex.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, PartialEq)]
+enum Token {
+    Number(i64),
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    LParen,
+    RParen,
+    EOF,
+}
+
+fn lex(input: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut iter = input.chars().peekable()
+
+    while let Some(ch) = iter.peek() {
+        match ch {
+            ch if ch.is_whitespace() => iter.next(),
+            '+' => { tokens.push(Token::Plus); iter.next(); }
+            '-' => { tokens.push(Token::Minus); iter.next(); }
+            '*' => { tokens.push(Token::Multiply); iter.next(); }
+            '/' => { tokens.push(Token::Divide); iter.next(); }
+            '(' => { tokens.push(Token::LParen); iter.next(); }
+            ')' => { tokens.push(Token::RParen); iter.next(); }
+        }
+    }
+}
